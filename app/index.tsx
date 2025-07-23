6e095aac15af1134d1eac7bafc57344366ead2da
alvensar22 <alven.academics@gmail.com>
2025-07-23 12:05:17 +0800

    added: assets, constants and fonts config

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import "./globals.css";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
+      <Text className="text-5xl font-quicksand-bold font-bold text-primary">
+        Food Delivery App!
       </Text>
     </View>
   );
